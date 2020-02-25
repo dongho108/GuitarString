e687b62912544b5700a7163157fc339fa1fe58ca
K_Boan <54317630+dongho108@users.noreply.github.com>
2020-02-25 21:44:42 +0900

    예외처리 찾지못했음

---
+++ main.cpp
@@ -0,0 +1,78 @@
+//
+//  main.cpp
+//  Guitar strings
+//
+//  Created by dongho on 2020/02/25.
+//  Copyright © 2020 dongho. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class GuitarString{
+    vector<vector<int> > brand;
+    int cutNum, brandNum;
+    
+public:
+    GuitarString(int c, int b){
+        cutNum = c;
+        brandNum = b;
+        //브랜드 수만큼 늘려주기
+        int temp1, temp2;
+        for(int i=0; i<brandNum; i++){
+            brand.push_back(vector<int>());
+            
+            cin >> temp1 >> temp2;
+            brand.at(i).push_back(temp1);
+            brand.at(i).push_back(temp2);
+            brand.at(i).push_back(findBMin(i));
+        }
+    }
+    int findBMin(int i){
+        int min;
+        int bundleEa, bundle, ea;
+        bundleEa = (cutNum/6 * brand.at(i).at(0) + ((cutNum % 6) * brand.at(i).at(1)));
+        
+        ea = cutNum * brand.at(i).at(1);
+        
+        bundle = (cutNum/6+1)*brand.at(i).at(0);
+        
+        if(bundleEa < ea){
+            if(bundleEa < bundle)
+                min = bundleEa;
+            else
+                min = bundle;
+        }
+        else{
+            if(ea < bundle)
+                min = ea;
+            else
+                min = bundle;
+        }
+        
+        return min;
+    }
+    
+    //브랜드의 최소값끼리 비교하기
+    void searchMin(){
+        int Min=0;
+        for(int i=0; i<brandNum-1; i++){
+            if(brand.at(i).at(2) < brand.at(i+1).at(2)){
+                Min = brand.at(i).at(2);
+            }
+            else
+                Min = brand.at(i+1).at(2);
+        }
+        cout << Min;
+    }
+    
+};
+
+int main() {
+    int cutNum, brandNum;
+    cin >> cutNum >> brandNum;
+    
+    GuitarString g(cutNum, brandNum);
+    g.searchMin();
+}
